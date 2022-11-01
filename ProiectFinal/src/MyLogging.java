c4022d136b9281a5f2cae79955b7c83b00f17b9c
ByRocking <116735604+ByRocking@users.noreply.github.com>
2022-11-01 21:37:04 +0200

    Update MyLogging.java

--- ProiectFinal/src/MyLogging.java
+++ ProiectFinal/src/MyLogging.java
@@ -1,8 +1,13 @@
 import java.io.File;
-public class MyLogging{
-    public MyLogging() {
-        File file = new File("Logging.txt");
-    }
-    static void scrieLogging(String string){}
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class MyLogging{
+    static FileWriter myWriter;
+    public MyLogging() throws IOException {
+        myWriter = new FileWriter("Logging.txt");
+    }
+    static void scrieLogging(String string) throws IOException {
+        myWriter.append(string);
+    }
 }
