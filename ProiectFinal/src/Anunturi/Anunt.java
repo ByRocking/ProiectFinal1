e2787c977d4fbaab700a2d99cb5abad599f212a8
Pogacean Marius <116893771+PogaceanMarius@users.noreply.github.com>
2022-11-01 20:00:46 +0200

    Update Anunt.java

--- ProiectFinal/src/Anunturi/Anunt.java
+++ ProiectFinal/src/Anunturi/Anunt.java
@@ -2,19 +2,19 @@ package Anunturi;
 
 public class Anunt {
 
-    public void static AnuntDeschidere() {
+    public void AnuntDeschidere() {
         System.out.println("Gradina zoologica s-a deschis publicului!");
     }
 
-    public void static AnuntInchidere() {
+    public void AnuntInchidere() {
         System.out.println("Gradina zoologica s-a inchis!");
     }
 
-    public void static AnuntLeiNoi() {
+    public void AnuntLeiNoi() {
         System.out.println("Gradina zoologica a primit o familie noua de lei!");
     }
 
-    public void static AnuntPestiNoi() {
+    public void AnuntPestiNoi() {
         System.out.println("Gradina zoologica a primit specii noi de pesti!");
     }
 }
