1f9c55d6df077e9ddd391962bc7084e656140961
rradu08 <116719926+rradu08@users.noreply.github.com>
2022-11-01 15:44:50 +0200

    Update Animale.java

--- ProiectFinal/src/Animale.java
+++ ProiectFinal/src/Animale.java
@@ -1,7 +1,7 @@
 public abstract class Animale
 {
     void trezirea() {
-        System.out.println("Animalul s-a trezit!");
+        System.out.println("Animalul s-a murit!");
     }
     void somn() {
         System.out.println("Animalul doarme!");
