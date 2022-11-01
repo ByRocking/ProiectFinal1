5a28e1ebfa1ab8b9b7ef27b42b3e63ac74290721
ByRocking <andrei22sofronia@gmail.com>
2022-11-01 15:24:49 +0200

    xgchjb

--- ProiectFinal/src/MainWindow.java
+++ ProiectFinal/src/MainWindow.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MainWindow {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//serdtgj
         ArrayList<Leu> leu = new ArrayList<Leu>();
         ArrayList<Peste> peste = new ArrayList<Peste>();
 
