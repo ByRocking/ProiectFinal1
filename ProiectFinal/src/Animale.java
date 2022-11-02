d280688783a488bc3eb6ef5ae3fb1d726f334960
ByRocking <116735604+ByRocking@users.noreply.github.com>
2022-11-02 11:29:56 +0200

    Update Animale.java

--- ProiectFinal/src/Animale.java
+++ ProiectFinal/src/Animale.java
@@ -1,4 +1,4 @@
-public abstract class Animale
+public abstract class Animale  //clasa abstracta (nu poate fi instantiata este doar o idee)
 {
     void trezirea() {
         System.out.println("Animalul s-a trezit!");
